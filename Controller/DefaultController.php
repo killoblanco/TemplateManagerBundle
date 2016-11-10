546ee67ba25a2aa78312b9d29db9000fd8c9efe9
Kamilo Vasquez <me@killoblan.co>
2016-11-09 23:25:11 -0500

    init commit

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace killoblanco\TemplateManagerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TemplateManagerBundle:Default:index.html.twig');
+    }
+}
---
+++ TemplateManagerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace killoblanco\TemplateManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TemplateManagerBundle extends Bundle
+{
+}
